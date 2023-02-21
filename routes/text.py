254a1d9244ff0a0fc2fc7f2e961174aa126173d0
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2023-02-21 00:09:35 -0600

    20/02/2023 Ya funciona 12:07 pm

--- routes/text.py
+++ routes/text.py
@@ -7,6 +7,7 @@ from os import getcwd
 from docs import tags_metadata
 import io
 from typing import List
+import re
 
 pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
 
@@ -39,8 +40,7 @@ async def submit_image(file: UploadFile = File(...),):
     id = conn.local.testimage.insert_one(image).inserted_id
     with Image.open(io.BytesIO(data)) as pic:
         texto = pytesseract.image_to_string(pic)
-        texto.replace('\\n', ' ')
-        texto.replace('\\t','  ')
+        texto.translate(' ','\\t\\n')
     return str({texto, id})
 
 @text.post('/submit_more', response_model= dict(), tags=["Text"])
