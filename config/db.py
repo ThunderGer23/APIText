7722a25450275c858144cbe7f1c8f6ddb4a7def9
ThunderGer <jaames11@hotmail.com>
2023-05-03 01:31:50 -0600

    Cambio para db

--- config/db.py
+++ config/db.py
@@ -2,4 +2,4 @@
 from pymongo import MongoClient
 from config.keys import MongoCli
 
-conn = MongoClient(MongoCli['docker'])
+conn = MongoClient("mongodb://mongo:w9nv2Uj41vovywuLLfV3@containers-us-west-190.railway.app:5921")
