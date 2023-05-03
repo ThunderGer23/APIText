f7424ba9738f90422d36cdb985533dd2b656e69a
ThunderGer <jaames11@hotmail.com>
2023-05-03 01:39:30 -0600

    Cambio para db

--- config/db.py
+++ config/db.py
@@ -1,5 +1,4 @@
 # Connecting to the MongoDB database.
 from pymongo import MongoClient
-from config.keys import MongoCli
 
 conn = MongoClient("mongodb://mongo:w9nv2Uj41vovywuLLfV3@containers-us-west-190.railway.app:5921")
